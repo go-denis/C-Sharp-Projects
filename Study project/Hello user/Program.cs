80a7b4d73c0df28bd2504b9f8441f92c362db0ba
Delfin-Go <go-denis@mail.ru>
2022-11-24 18:36:42 +0300

    Домашние работы по семинару

--- Study project/Hello user/Program.cs
+++ Study project/Hello user/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Введите ваше имя: ");
 string username = Console.ReadLine();
+int a = new Random().Next(1, 1000);
+
 Console.WriteLine("Привет! ");
-Console.WriteLine(username);
+Console.WriteLine(a);
