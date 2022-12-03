15ea8d7859697b9f3c13ce49f513eb0efc29fc0d
Delfin-Go <go-denis@mail.ru>
2022-12-03 23:07:21 +0300

    Задание 25 по семинару 4

---
+++ sem_4_dz1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+НЕ ИСПОЛЬЗОВАТЬ Math.Pow
+*/
+
+class Dz_25{
+      static void Main(string[] args)
+    {
+        Console.WriteLine("Привет! Это задача 25, введи два числа и первое будет возведено в степень второго");
+
+        Console.WriteLine("Введите первое число:");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите второе число:");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine(dz_25_run(a,b));
+    }
+
+    static int dz_25_run(int a, int b){
+
+        int step = a;
+        for (int i = 1; i < b; i++)
+        {
+           step = step * a;
+        }
+
+        return step;
+    }
+}
