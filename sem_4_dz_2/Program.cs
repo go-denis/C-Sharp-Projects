dee076e998f35388ff7afd095b5d52db6f495d9a
Delfin-Go <go-denis@mail.ru>
2022-12-04 00:25:50 +0300

    Программа для дз 27, семинар 4

---
+++ sem_4_dz_2/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+class Dz_27{
+      static void Main()
+    {
+      Console.Clear();
+      Console.Write("Эта программа складыввает цифры в числе!");
+      Console.Write("Введите число N: ");
+      int chislo = Convert.ToInt32(Console.ReadLine());
+      int sumchislo = Sumchislo(chislo);
+      Console.WriteLine("Сумма цифр в числе: " + sumchislo);
+    }
+
+    static int Sumchislo(int chislo){
+    
+    int counter = Convert.ToString(chislo).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = chislo - chislo % 10;
+      result = result + (chislo - advance);
+      chislo = chislo / 10;
+    }
+   return result;
+  }
+}
