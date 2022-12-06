e679a5fc6aaf3a17d0c1c5aeff68a56b2f70b871
Delfin-Go <go-denis@mail.ru>
2022-12-06 20:43:35 +0300

    Семинар 5, Задача 36 Ген масс + Нахождение разницы между макс и мин элементов массива

---
+++ sem_5_dz_3/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76
+*/
+
+Console.Clear();
+
+Console.WriteLine("Напишите какой должна быть длинна массива:");
+int size = int.Parse(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив готов: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int z = 0; z < numbers.Length; z++)
+{
+    if (numbers[z] > max)
+        {
+            max = numbers[z];
+        }
+    if (numbers[z] < min)
+        {
+            min = numbers[z];
+        }
+}
+
+Console.WriteLine($"Всего {numbers.Length} чисел. Максимальное значение: {max}, минимальное значение: {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением: {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
