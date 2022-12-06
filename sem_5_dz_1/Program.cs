9cfefdf7ebdfdb338ff397a85ef70b2ada14fb20
Delfin-Go <go-denis@mail.ru>
2022-12-06 20:30:45 +0300

    Семинар 5 задача 34 генерация массива и нахождение четных чисел

---
+++ sem_5_dz_1/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+Console.Clear();
+Console.WriteLine("Напишите какой должна быть длинна массива:");
+Console.WriteLine("Напишите какой должна быть длинна массива:");
+int size = int.Parse(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Сформированный массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"Всего {numbers.Length} чисел, {count} из них чётные");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
