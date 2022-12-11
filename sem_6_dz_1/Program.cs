d379b4dda91cc75a2a81f8067b5e132d02805da2
Delfin-Go <go-denis@mail.ru>
2022-12-11 17:44:47 +0300

    Задачка 41 к семинару 6. Готово

---
+++ sem_6_dz_1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Clear();
+Console.WriteLine("Введите сколько угодно числел, через запятую: ");
+int[] mass = Array.ConvertAll(Console.ReadLine().Split(','), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < mass.Length; i++)
+{
+    if (mass[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Введенных числах: {count}, числе которые больше 0");
